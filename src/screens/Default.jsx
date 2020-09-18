35bb998378f38d5e8edd450db9f72dc4b9301f22
Andrew <andrewk745@gmail.com>
2020-09-18 17:40:21 -0400

    Added default component

---
+++ src/screens/Default.jsx
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react'
+
+export default class Default extends Component {
+    render() {
+        return (
+            <div>
+                <h1 className="title has-text-centered is-capitalized">404 error page not found</h1>
+                <h3 className="subtitle has-text-centered">The requested URL<span className="has-text-danger">{this.props.location.pathname}</span> was not found</h3>
+        </div>
+        )
+    }
+}
