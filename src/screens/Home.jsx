c29fb7bb740dee90839c5a0d9701553270949641
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-09-19 13:28:22 -0400

    🖥 - Added Screens

---
+++ src/screens/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Title } from '../components'
+
+export const Home = () => {
+    return (
+        <div>
+            <Title title='hello world' color='primary' alignment='centered' className='mt-5'/>
+        </div>
+    )
+}
---
+++ src/screens/index.js
@@ -0,0 +1,3 @@
+export * from './Home'
+export * from './Team'
+export * from './Default'
