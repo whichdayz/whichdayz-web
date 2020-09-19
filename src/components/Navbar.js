b271f72b2eda23c3f3f218428d1a656e432d9346
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-09-19 19:00:51 -0400

    📡 -  Completed Navbar Component

---
+++ src/components/Navbar.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import logo from '../images/icon.png'
+import { Button } from './Button'
+import { Hamburger } from './Hamburger'
+
+export const Navbar = () => {
+    const [hamburger, setHamburger] = useState(false)
+    const [hamburgerName, setHamburgerName] = useState('')
+    const changeClassName  = () => {
+        setHamburger(!hamburger)
+        return hamburger ? setHamburgerName('is-active') : setHamburgerName('')
+    }
+    return (
+        <nav className="navbar is-spaced is-fixed-top is-primary" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+                <Link to='/' className='navbar-item'>
+                    <img 
+                        src={logo} 
+                        alt='logo' 
+                        style={{maxHeight: '2.5em', display: 'block',margin: 'auto', borderRadius: '40%'}}
+                    />
+                </Link>
+                <Hamburger 
+                    changeClassName={changeClassName} 
+                    hamburgerName={hamburgerName} 
+                />
+            </div>
+            <div className={`${hamburgerName} navbar-menu`}>
+                <div className="navbar-start">
+                    <Link to='/' className='navbar-item'>Home</Link>
+                    <Link to='/team' className='navbar-item'>Team</Link>
+                    <div className="navbar-item has-dropdown is-hoverable">
+                        <div className="navbar-link">
+                        More
+                        </div>
+                        <div className="navbar-dropdown">
+                        <Link to='/about' className='navbar-item'>About</Link>
+                        <Link to='/jobs' className='navbar-item'>Jobs</Link>
+                        <Link to='/contact' className='navbar-item'>Contact</Link>
+                            <hr className="navbar-divider"></hr>
+                        <Link to='/report' className='navbar-item'>Report an issue</Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="navbar-end">
+                    <div className="navbar-item">
+                        <div className="buttons">
+                            <Button title='Sign In' className='is-light has-text-weight-bold has-text-centered' color='primary'/>
+                            <Button title='Get Started' className='has-text-weight-bold has-text-centered' color='success'/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
