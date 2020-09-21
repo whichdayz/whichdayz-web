9fc037abf5ca118709ffac357f81196611c53816
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-09-21 16:12:28 -0400

    ⛅️ - Home and Landing page routing complete

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import { Switch, Route } from "react-router-dom";
+import { AuthContext } from '../contexts/AuthContext'
 import { Default } from '../screens'
 import Home from '../screens/Home/Home';
 import About from '../screens/About/About';
@@ -7,13 +8,15 @@ import Team from '../screens/Team/Team';
 import FirebaseSignIn from '../screens/Firebase/FireBaseSignIn'
 import Login from '../screens/Login/Login'
 import SignUp from '../screens/SignUp/SignUp'
+import { Landing } from '../screens/Landing/Landing';
 
 
 export const Routes = () => {
+    const { currentUser } = useContext(AuthContext)
     return (
         <Switch>
             {/* Home Page */}
-            <Route exact path='/' component={Home}/>
+            <Route exact path='/' render={props => (currentUser ? <Home/> : <Landing/>)}/>
             <Route exact path='/login' component={Login}/>
             <Route exact path='/join' component={SignUp}/>
 
