4f559a16a4d3d01cb1234b232a918bec28fb3d61
Giusseppe Del Vecchio <giudelvecchio@gmail.com>
2020-09-23 01:44:05 -0400

    🌮 - Added About component into Landing page

--- src/screens/Landing/Landing.jsx
+++ src/screens/Landing/Landing.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import About from '../About/About'
 
 export const Landing = () => {
     return (
         <div>
-            This is the landing page
+            <About/>
         </div>
     )
 }
